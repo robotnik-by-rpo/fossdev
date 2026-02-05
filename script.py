db251ee6eb1c3cf5093125d0ba1f8966a7173c9c
Monke <chikindin99@inbox.ru>
2026-02-05 14:35:45 +0800

    Added type

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
-def sum(a: int, b: int):
+def sum(a: int, b: int)->int:
     return a+b
 
-def div(a: int, b: int):
+def div(a: int, b: int)->int:
     if b != 0:
         raise ValueError("Denom is zero")
     return a/b
