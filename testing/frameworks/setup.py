e0fc62683983167de5dad390d1519c439d25b7b7
Monke <chikindin99@inbox.ru>
2026-03-05 15:18:52 +0800

    Added pytest and unittest

--- testing/frameworks/setup.py
+++ testing/frameworks/setup.py
@@ -4,5 +4,6 @@ setup(
     name="tax",
     version="0.0.0",
     package_dir={"": "src"},
+    install_requires=[],
     packages=find_packages(where="src")
 )
--- testing/frameworks/src/tax/income.py
+++ testing/frameworks/src/tax/income.py
@@ -1,5 +1,2 @@
-def calculate_tax(income):
-    if income < 0:
-        raise ValueError("Could not have negative income")
-    
-    return int(income * 0.15*100) / 100
+def calculate_tax(income):    
+    return int(income * 0.13*100) / 100
