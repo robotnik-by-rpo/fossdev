75f9db952e882a20bfb03dd264046acfd91fd7f6
Monke <chikindin99@inbox.ru>
2026-03-10 15:04:23 +0800

    Refatored ndfl calculation

--- testing/tdd/src/ndfl.py
+++ testing/tdd/src/ndfl.py
@@ -1,13 +1,16 @@
 def calculate_ndfl(income):
     result = 0
-    if income < 2_400_000:
-        result = income * 0.13
-    elif income < 5_000_000:
-        result = (2_400_000 * 0.13 + (income - 2_400_000) * 0.15)
-    elif income < 20_000_000:
-        result = (2_400_000 * 0.13 + 2_600_000 * 0.15 + (income - 5_000_000) * 0.18)
-    elif income < 50_000_000:
-        result = (2_400_000 * 0.13 + 2_600_000 * 0.15 + 15_000_000 * 0.18 + (income - 20_000_000) * 0.2)
-    elif income > 50_000_000:
-        result = (2_400_000 * 0.13 + 2_600_000 * 0.15 + 15_000_000 * 0.18 + 30_000_000 * 0.2 + (income - 50_000_000) * 0.22)
-    return result
+
+    tiers = (
+        (0,0,0.13),
+        (2_400_000, 312_000, 0.15),
+        (5_000_000,702_000,0.18),
+        (20_000_000, 3_402_000, 0.2),
+        (50_000_000, 9_402_000, 0.22)
+    )
+
+    for start, addition, taxrate in tiers[::-1]:
+        if income > start:
+            return (income - start) * taxrate + addition
+    raise RuntimeError("Error in tax calculation")
+            
--- testing/tdd/tests/test_basic_ndfl.py
+++ testing/tdd/tests/test_basic_ndfl.py
@@ -4,13 +4,13 @@ from ndfl import calculate_ndfl
 # TODO make test to 
 
 def test_ndfl_tier_1_basic():
-    assert calculate_ndfl(2_000_000) == 200_200
+    assert calculate_ndfl(2_000_000) == 260_200
 
 def test_ndfl_tier_2_basic():
     assert calculate_ndfl(4_000_000) == 552_000
 
 def test_ndfl_tier_3_basic():
-    assert calculate_ndfl(10_000_000) == 1_002_000
+    assert calculate_ndfl(10_000_000) == 1_602_000
 
 def test_ndfl_tier_4_basic():
     assert calculate_ndfl(30_000_000) == 5_402_000
