c9f8f48f3f2b5d55b7eb149100b416e7fb1489a1
Monke <chikindin99@inbox.ru>
2026-03-10 14:43:36 +0800

    Added if else block in func  calculate_ndfl

--- testing/tdd/src/ndfl.py
+++ testing/tdd/src/ndfl.py
@@ -1,2 +1,9 @@
 def calculate_ndfl(income):
-    return income*0.13
+    result = 0
+
+    if income  < 2_400_000:
+        result = income * 0.13
+    else:
+
+        result = income * 0.15 + (income - 2_400_000)*0.15
+    return result
