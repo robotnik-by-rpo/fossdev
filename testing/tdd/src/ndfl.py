db5f27d3ee92cd251ed0e5dd27aee0e53809b300
Monke <chikindin99@inbox.ru>
2026-03-10 14:53:09 +0800

    Added prograce ndfl

--- testing/tdd/src/ndfl.py
+++ testing/tdd/src/ndfl.py
@@ -2,8 +2,20 @@ def calculate_ndfl(income):
     result = 0
 
     if income  < 2_400_000:
-        result = income * 0.13
-    else:
-
-        result = income * 0.15 + (income - 2_400_000)*0.15
+        result += income * 0.13
+    elif income > 2_400_000:
+        result = (
+            2_400_000*0.13 + 
+            (income - 2_400_000) *0.15
+        )
+    elif income < 20_000_000:
+        result = (
+            2_400_000*0.13+2_600_000*0.15+15_000_000*0.18 + 
+            (income - 20_000_000) * 0.2
+        )
+    elif income > 50_000_000:
+        result = (
+            2_400_000*0.13+2_600_000+15_000_000*0.18 + 30_000_000*0.2+
+            (income - 50_000_000) * 0.22
+        )
     return result
