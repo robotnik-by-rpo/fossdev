0412609a4525dc457217a420201968210903c8f0
Monke <chikindin99@inbox.ru>
2026-03-10 14:35:42 +0800

    Added tests for basic ndfl calculation

---
+++ testing/tdd/tests/test_basic_ndfl.py
@@ -0,0 +1,22 @@
+from ndfl import calculate_ndfl
+
+
+# TODO make test to 
+
+def test_ndfl_tier_1_basic():
+    assert calculate_ndfl(2_000_000) == 200_200
+
+def test_ndfl_tier_2_basic():
+    assert calculate_ndfl(4_000_000) == 552_000
+
+def test_ndfl_tier_3_basic():
+    assert calculate_ndfl(10_000_000) == 1_002_000
+
+def test_ndfl_tier_4_basic():
+    assert calculate_ndfl(30_000_000) == 5_402_000
+
+def test_ndfl_tier_5_basic():
+    assert calculate_ndfl(60_000_000) == 11_602_000
+
+
+
