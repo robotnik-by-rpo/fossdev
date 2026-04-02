17fd70782ab01a270f4fa77648f764a46e30a473
Monke <chikindin99@inbox.ru>
2026-04-02 04:01:08 +0000

    Added fix setup

--- makeutil/util/setup.py
+++ makeutil/util/setup.py
@@ -5,7 +5,7 @@ setup(
    version='1.0',
    description="It's util for http request. It check url and return status code. You can use flag to get more information about url",
    license='MIT',
-   author='Chikindin Ilya',
+   author='robotnik-by-rpo',
    author_email='chikindin99@inbox.ru',
    url='https://github.com/robotnik-by-rpo/fossdev/tree/homework/makeutil-pypi',
    packages=['util'], 
@@ -14,6 +14,7 @@ setup(
         'test': [
             'pytest',
             'coverage',
+            'requests'
         ],
    },
    python_requires='>=3',
